13f205481214e2db44bb1f793a3dd0ca404a5b4d
chiehenwang <71625921+chiehenwang@users.noreply.github.com>
2022-04-10 16:07:06 +0800

    Readhalf file and create the structure of  netlist
    Readhalf file and create the structure of  netlist

---
+++ readfile.h
@@ -0,0 +1,46 @@
+#ifndef _READFILE_H_
+#define _READFILE_H_
+
+    typedef struct _cor{ //record coordinate
+        int x;
+        int y;
+    }coordinate;
+
+    typedef struct _standard_cell{
+        int number;
+        int size_x;
+        int size_y;
+        int num_of_pin;
+        coordinate *pinarray;
+    }cell;
+
+    typedef struct _die_information{
+        int size_x;
+        int size_y;
+        int row_height;
+        int row_number;
+        double MaxUtil;
+        char *tech;
+    }Die;
+
+    typedef struct _terminal{
+        int size_x;
+        int size_y;
+        int spacing;
+    }hybrid_terminal;
+
+    typedef struct _Libcell{
+        char *libcell_name; //MC1,MC2...
+        int size_x;
+        int size_y;
+        int pin_count;
+        coordinate *pinarray;
+    }Libcell;
+
+    typedef struct _tech_menu{
+        char *tech; //TA,TB
+        int libcell_count;
+        Libcell *libcell; 
+    }tech_menu;
+
+#endif
