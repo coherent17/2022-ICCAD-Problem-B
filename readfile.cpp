c634e67a13341541e32f873d8ed8e32149290913
coherent <mnb51817@gmail.com>
2022-04-10 17:22:29 +0800

    temporary storage

--- readfile.cpp
+++ readfile.cpp
@@ -5,11 +5,13 @@
 Die *createDie(){
     Die *newDie = malloc(sizeof(Die));
     newDie->tech = malloc(sizeof(char)*3);
+    return newDie;
 }
 
 tech_menu *create_tech_menu(){
     tech_menu *new_tech_menu = malloc(sizeof(tech_menu));
     tech_menu->tech = malloc(sizeof(char)*3);
+    return new_tech_menu;
 }
 
 void create_libcell(Libcell *libcell, int libcell_count){
@@ -19,11 +21,28 @@ void create_libcell(Libcell *libcell, int libcell_count){
     }
 }
 
+void create_pinarray(Pin *pinarray, int pin_count){
+    pinarray = malloc(sizeof(Pin) * pin_count);
+    for(int i = 0; i < pin_count; i++){
+        pinarray[i]->pin_name = malloc(sizeof(char)*5);
+    }
+}
+
+InstanceNode *create_InstanceArray(int NumInstances){
+    InstanceNode *Instancearray = malloc(sizeof(InstanceNode) * NumInstances);
+    for(int i = 0; i < NumInstances; i++){
+        Instancearray[i]->instance_name = malloc(sizeof(char) * 5);
+        Instancearray[i]->tech = malloc(sizeof(char) * 5);
+        Instancearray[i]->standard_cell_manufacture = malloc(sizeof(char) * 7);
+    }
+    return Instancearray;
+}
+
 void read_one_blank_line(FILE *input){
     fscanf(input,"%*s");
 }
 
-void read_die_information(char *input_name,Die *top_die,Die *bottom_die,hybrid_terminal *term,int *NumTechnologies){
+void readfile(char *input_name,Die *top_die,Die *bottom_die,hybrid_terminal *term,int *NumTechnologies, tech_menu **tech_menu, int *NumInstances, InstanceNode *Instancearray, int *NumNets){
     FILE *input=fopen(input_name,"r");
     
     //read DieSize
@@ -60,7 +79,7 @@ void read_die_information(char *input_name,Die *top_die,Die *bottom_die,hybrid_t
     //read different tech
 
     //allocate memory for the tech menu
-    tech_menu **tech_menu = malloc(sizeof(tech_menu *) * (*NumTechnologies));
+    tech_menu = malloc(sizeof(tech_menu *) * (*NumTechnologies));
     for(int i=0 ; i < *NumTechnologies ; i++){
         tech_menu[i]= create_tech_menu();
     }
@@ -71,13 +90,30 @@ void read_die_information(char *input_name,Die *top_die,Die *bottom_die,hybrid_t
 
         //read libcell libcell count time
         for(int j = 0; j < tech_menu[i]->libcell_count; j++){
-            Libcell *curcell = tech_menu[i]->libcell;
-            fscanf(input, "%*s %s %d %d %d", curcell[j]->);
+            Libcell *curlibcell = tech_menu[i]->libcell[j];
+            fscanf(input, "%*s %s %d %d %d", curlibcell->libcell_name, &curlibcell->size_x, &curlibcell->size_y, &curlibcell->pin_count);
+            create_pinarray(curlibcell->pinarray, curlibcell->pin_count);
+
+            //read pinarray pinarray_count time
+            for(int k = 0; k < curlibcell->pin_count; k++){
+                Pin *curPin = curlibcell->pinarray[k];
+                fscanf(input, "%*s %s %d %d", curPin->pin_name, curPin->x, curPin->y);
+            }
         }
     }
+    read_one_blank_line(input);
 
+    //read standard cell belong to which manufacture
+    fscanf(input, "%*s %d", &(*NumInstances));
+    InstanceNode *Instancearray = create_InstanceArray(int NumInstances);
+    for(int i = 0; i < *NumInstances; i++){
+        fscanf(input, "%*s %s %s", Instancearray[i]->instance_name, Instancearray[i]->standard_cell_manufacture);
+    }
+    read_one_blank_line(input);
 
-
+    //read the circuit netlist
+    fscanf(input, "%*s %d", &(*NumNets));
+    
 }
 
 
--- readfile.h
+++ readfile.h
@@ -29,12 +29,18 @@
         int spacing;
     }hybrid_terminal;
 
+    typedef struct _pin{
+        char *pin_name; //P1, P2, ...
+        int x;
+        int y;
+    }Pin;
+
     typedef struct _Libcell{
         char *libcell_name; //MC1,MC2...
         int size_x;
         int size_y;
         int pin_count;
-        coordinate *pinarray;
+        Pin *pinarray;
     }Libcell;
 
     typedef struct _tech_menu{
@@ -42,5 +48,19 @@
         int libcell_count;
         Libcell *libcell; 
     }tech_menu;
+
+    typedef struct _InstanceNode{
+        char *instance_name;  //C1, C2, ...
+        char *tech;           //TA, TB, ...
+        char *standard_cell_manufacture; //MC1, MC2, ...
+    }InstanceNode;
+
+    typedef struct _Net{
+
+    }
+
+    typedef struct _Net{
+        
+    }Net;
 
 #endif
